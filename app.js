82a39a7117f946c161d40ea3404338eac3b3a7f0
Unknown <gregojquiros@gmail.com>
2017-05-07 19:27:35 +0200

    Updated session middleware

--- app.js
+++ app.js
@@ -30,14 +30,24 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('Tracks Musirac'));
 app.use(methodOverride('_method'));
-app.use(session());
+app.use(session({
+  secret: process.env.SECRET || "cosamuysecreta",
+  store: store,
+  resave: false,
+  saveUninitialized: false,
+  proxy: true,
+  name: "MusIRAC",
+  cookie: {
+    secure: false,
+    maxAge: config.sessionExpiration || 60000
+  }
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Helpers dinamicos:
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   // si no existe lo inicializa
   if (!req.session.redir) {
     req.session.redir = '/';
@@ -51,7 +61,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/media/:nombre', function(req, res) {
+app.get('/media/:nombre', function (req, res) {
   var song = path.join(__dirname, 'media', req.params.nombre);
   mediaserver.pipe(req, res, song);
 });
@@ -59,7 +69,7 @@ app.get('/media/:nombre', function(req, res) {
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -70,7 +80,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -81,7 +91,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
